79137f0b3823fa8bde4c377e38f7f3fce50d5b91
webuiexpert <info@thecsshouse.com>
2025-07-03 12:11:19 +0530

    All Changes Done
    All Changes Done

--- src/Components/TrustPilotMarquee.jsx
+++ src/Components/TrustPilotMarquee.jsx
@@ -1,6 +1,7 @@
 import trustpilotimg1  from "../assets/trust-pilot-img1.png";
 import trustpilotimg2  from "../assets/trust-pilot-img2.png";
 import trustpilotimg3  from "../assets/trust-pilot-img3.png";
+import trustpilotimg14  from "../assets/trust-pilot-img14.png";
 import trustpilotimg4  from "../assets/trust-pilot-img4.png";
 import trustpilotimg5  from "../assets/trust-pilot-img5.png";
 import trustpilotimg6  from "../assets/trust-pilot-img6.png";
@@ -11,11 +12,10 @@ import trustpilotimg10  from "../assets/trust-pilot-img10.png";
 import trustpilotimg11  from "../assets/trust-pilot-img11.png";
 import trustpilotimg12  from "../assets/trust-pilot-img12.png";
 import trustpilotimg13  from "../assets/trust-pilot-img13.png";
-import trustpilotimg14  from "../assets/trust-pilot-img14.png";
 
 function TrustPilotMarquee() {
 
-    const trustImages = [trustpilotimg1, trustpilotimg2, trustpilotimg3, trustpilotimg4, trustpilotimg5, trustpilotimg6, trustpilotimg7, trustpilotimg8, trustpilotimg9, trustpilotimg10, trustpilotimg11, trustpilotimg12, trustpilotimg13, trustpilotimg14]
+    const trustImages = [trustpilotimg1, trustpilotimg2, trustpilotimg3, trustpilotimg14, trustpilotimg4, trustpilotimg5, trustpilotimg6, trustpilotimg7, trustpilotimg8, trustpilotimg9, trustpilotimg10, trustpilotimg11, trustpilotimg12, trustpilotimg13]
 
   return (
     <div className="overflow-hidden md:py-16 whitespace-nowrap bg-[radial-gradient(circle,rgba(2,33,80,1)_6%,rgba(0,0,0,1)_47%)] py-10">
