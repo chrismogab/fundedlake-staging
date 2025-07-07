999aecad59b222abc5e357822063b98fd2bdddbb
webuiexpert <webuiexpert929@gmail.com>
2025-07-07 11:28:57 +0530

    add a certificate image

--- src/Components/CertificateMarqueeSlider.jsx
+++ src/Components/CertificateMarqueeSlider.jsx
@@ -1,3 +1,4 @@
+import img7 from '../assets/certificate-of-withdrawl7.jpg'; 
 import img1 from '../assets/certificate-of-withdrawl1.jpeg';
 import img2 from '../assets/certificate-of-withdrawl2.jpeg';
 import img3 from '../assets/certificate-of-withdrawl3.jpeg'; 
@@ -6,13 +7,14 @@ import img5 from '../assets/certificate-of-withdrawl5.jpeg';
 import img6 from '../assets/certificate-of-withdrawl6.jpeg'; 
 
 
+
 const CertificateMarqueeSlider = () => {
-  const images = [img1, img2, img3, img4, img5, img6];
+  const images = [img7, img1, img2, img3, img4, img5, img6];
 
   return (
     <div className="overflow-hidden lg:py-24 md:py-10 whitespace-nowrap bg-[radial-gradient(circle,rgba(2,33,80,1)_6%,rgba(0,0,0,1)_47%)] py-4">
       <div className="marquee-container relative">
-        <div className="marquee">
+        <div className="marquee w-[100%]">
         {/* Duplicate for seamless loop */}
         {[...images, ...images].map((img, idx) => (
           <img
