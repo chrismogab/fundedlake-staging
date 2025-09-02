df958c00088decf29d2039fb453781caa2990878
webuiexpert <info@thecsshouse.com>
2025-09-02 13:48:48 +0530

    Hide the slider images
    Just because these are not final images

--- src/Components/BecomePartnerSlider.jsx
+++ src/Components/BecomePartnerSlider.jsx
@@ -3,9 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import heroImg0 from "../assets/forex-expo-imgs/forex-expo-img1.png"
-import heroImg1 from "../assets/forex-expo-imgs/forex-expo-img2.png"
-import heroImg2 from "../assets/forex-expo-imgs/forex-expo-img3.png"
-import heroImg3 from "../assets/forex-expo-imgs/forex-expo-img4.png"
+//import heroImg1 from "../assets/forex-expo-imgs/forex-expo-img2.png";
+//import heroImg2 from "../assets/forex-expo-imgs/forex-expo-img3.png";
+//import heroImg3 from "../assets/forex-expo-imgs/forex-expo-img4.png";
 
 function BecomePartnerSlider() {
   const settings = {
@@ -23,9 +23,9 @@ function BecomePartnerSlider() {
     <div className="w-full md:max-w-2xl mx-auto">
       <Slider {...settings}>
         <div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg0} alt="" /></div>
-        <div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg1} alt="" /></div>
+        {/*<div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg1} alt="" /></div>
         <div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg2} alt="" /></div>
-        <div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg3} alt="" /></div>
+        <div className="rounded-[24px] overflow-hidden"><img className="w-[100%] rounded-[24px]" src={heroImg3} alt="" /></div>*/}
   
       </Slider>
     </div>
